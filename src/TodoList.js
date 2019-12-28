73c9b799821afa273d167d7bf2978f99ddeb8385
Appsius <ilyas.ustunkaya234434@gmail.com>
2019-12-28 15:33:27 +0300

    Small Style Tweaks

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
-// import Divider from '@material-ui/core/Divider';
+import Divider from '@material-ui/core/Divider';
 import Todo from './Todo';
 
 function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
   return (
     <Paper>
       <List>
-        {todos.map(todo => (
+        {todos.map((todo, i) => (
           <>
             <Todo
               id={todo.id}
@@ -19,6 +19,7 @@ function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
               toggleTodo={toggleTodo}
               editTodo={editTodo}
             />
+            {i < todos.length - 1 && <Divider />}
           </>
         ))}
       </List>
